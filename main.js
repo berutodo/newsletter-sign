9af71b886c3ffdad4349cdb09218547d1d4ac58e
Gabriel <estudo.gabmartts@gmail.com>
2023-09-05 14:01:31 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function closeModal() {
+    let modal = document.getElementById("modal")
+    modal.style.display = "none"
+}
+
+function confirm() {
+    let modal = document.getElementById("modal")
+    modal.style.display = "flex"
+}
---
+++ output.cs
@@ -0,0 +1,32 @@
+* {
+  padding: 0;
+  margin: 0;
+  font-family: "Roboto", sans-serif;
+}
+
+.list {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+.list span {
+  display: flex;
+}
+
+.main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  width: 90%;
+  height: 40px;
+  border-radius: 5px;
+  border-style: none;
+  color: hsl(0, 0%, 100%);
+  font-size: small;
+}
+
+/*# sourceMappingURL=output.cs.map */
